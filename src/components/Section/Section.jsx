11a42d303cd830e49177651b2d4aa6e75526a377
Yakiv87 <130331039+Yakiv87@users.noreply.github.com>
2023-11-13 00:30:29 -0500

    sum

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import css from './Section.module.css'
 
 const Section = ({ title, children }) => {
 	return (
-		<>
-			<h2>{title}</h2>
+		<section className={css.container}>
+			<h2 className={css.title}>{title}</h2>
 			{children}
-		</>
+		</section>
 	);
 };
 
